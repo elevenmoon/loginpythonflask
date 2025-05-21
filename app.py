98cb62a4b4732ea86ac4b8b9a7a949207b84db50
Roman Edgardo Lugo Garcia <nigthmare_96@hotmail.com>
2025-05-20 22:41:46 -0400

    login flask desarrollo

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask
+from flask import render_template, redirect, request, Response, session
+from flask_mysqldb import MySQL, MySQLdb
+
+app = Flask(__name__, template_folder='template')
+
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASWOORD']=''
+app.config['MYSQL_DB']='freedb_condipastel'
+app.config['MYSQL_CURSORCLASS']='DictCursor'
+
+mysql=MySQL(app)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+#fucion de login
+@app.route('/acceso-login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST' and 'txtCorreo' in request.form and 'txtPassword':
+        _correo = request.form['txtCorreo']
+        _password = request.form['txtPassword']
+
+        cur = mysql.connection.cursor()
+        cur.execute('select * from users where user = %s and password = %s',(_correo,_password,))
+        account = cur. fetchone()
+
+        if account:
+            session['logueado'] = True
+            session['id'] = account['id']
+            return render_template("admin.html")
+        else:         
+            return render_template('index.html',mensaje= 'usuario incorrecto')
+
+if __name__ == '__main__':
+    app.secret_key = 'roman_hds'
+    app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
